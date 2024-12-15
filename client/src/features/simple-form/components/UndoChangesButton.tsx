4417432b783d0ec34a9aa55c8d25140878c41ac0
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-15 20:21:55 +1000

    footnotes for undochanges button, searchable via foot emoji

--- client/src/features/simple-form/components/UndoChangesButton.tsx
+++ client/src/features/simple-form/components/UndoChangesButton.tsx
@@ -12,12 +12,12 @@ export default function UndoChangesButton<T extends FieldValues>(props: UndoChan
 
   return (
     <InputAdornment position="end">
-      <Tooltip title="Undo changes" placement="right" arrow>
+      <Tooltip title="Undo changes" placement="right" enterDelay={700} arrow>
         <IconButton
           edge="end"
           size="small"
-          onClick={() => resetField(fieldName)}
           aria-label="Undo changes, reverting to what you originally had"
+          onClick={() => resetField(fieldName)}
         >
           <UndoIcon />
         </IconButton>
@@ -25,3 +25,12 @@ export default function UndoChangesButton<T extends FieldValues>(props: UndoChan
     </InputAdornment>
   );
 }
+
+/**
+ * 🦶🏻 FOOTNOTES:
+ *  - Uses RHF context ∴ must be inside <FormProvider>
+ *  - For UX, delay showing the tooltip to prevent constant flashing all over the screen (as many fields = many undo buttons)
+ *  - The user will quickly learn what this does as its effect is immediate
+ *  - For UX, use this button alone inside the <TextField> (there should be other UI feedback for pending changes, errors, etc)
+ *  - What about a button to clear the field? Don't be lazy: Ctrl+A & DELETE
+ */
