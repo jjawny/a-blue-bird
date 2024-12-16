a7cb928d8c28d789327f8a08e07e2763e7b831db
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-16 23:01:20 +1000

    wrap simple form example in a paper

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import { Paper } from "@mui/material";
 import SimpleFormExample from "~/features/simple-form/components/SimpleFormExample";
 
 function App() {
-  return <SimpleFormExample />;
+  return (
+    <Paper>
+      <SimpleFormExample />
+    </Paper>
+  );
 }
 
 export default App;
