2db4ba5b3c57f5485a4ce5b9c6f0e2fac0bbcb1a
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-16 23:01:28 +1000

    mui theme

---
+++ client/src/shared/constants/theme.ts
@@ -0,0 +1,49 @@
+import { createTheme } from "@mui/material/styles";
+
+/**
+ * GOTCHA:
+ *  - When applying theme <ThemeProvider>, import from @mui/material and NOT @emotion/react otherwise defaultProps will be ignored
+ *  - See https://github.com/mui/material-ui/issues/42892#issuecomment-2220244399
+ */
+export const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#2291cb",
+    },
+    secondary: {
+      main: "#24607F",
+      dark: "#1F516F",
+    },
+  },
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        variant: "outlined",
+        size: "small",
+        margin: "dense",
+      },
+      styleOverrides: {
+        root: {
+          "& .MuiOutlinedInput-root": {
+            borderBottomStyle: "inset",
+            borderBottomWidth: "2px",
+            borderBottomColor: "lightgray",
+          },
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          padding: 10,
+        },
+      },
+    },
+    MuiTooltip: {
+      defaultProps: {
+        arrow: true,
+      },
+    },
+  },
+});
