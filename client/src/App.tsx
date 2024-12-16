9ce11d67c6d6baa05d3a9fc11bfba4e69da5848c
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-16 23:46:46 +1000

    separate own components imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { Paper } from "@mui/material";
+// OWN COMPONENTS
 import SimpleFormExample from "~/features/simple-form/components/SimpleFormExample";
 
 function App() {
   return (
     <Paper>
+      <h1 className="mb-5 w-60 text-start">a boring form</h1>
       <SimpleFormExample />
     </Paper>
   );
