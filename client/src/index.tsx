97c43d3deaab4c4b70bc418699eef2710d9791d6
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-16 23:01:35 +1000

    use theme

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,15 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { theme } from "./shared/constants/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
