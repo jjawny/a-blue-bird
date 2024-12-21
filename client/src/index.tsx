9f3afc4aec80ec4b4a3be6d8ee44ef960f11b491
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-21 10:56:52 +1000

    tanstack router setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,26 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { theme } from "./shared/constants/theme.ts";
+import "~/index.css";
+import { routeTree } from "~/routeTree.gen";
+import { theme } from "~/shared/constants/theme.ts";
+
+//#region 🤘🏻 tanstack router setup
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+//#endregion
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>,
 );
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,15 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    /* 🤘🏻 tanstack router setup */
+    TanStackRouterVite(), //
+  ],
   resolve: {
     /* 🤘🏻 path alias */
     alias: {
