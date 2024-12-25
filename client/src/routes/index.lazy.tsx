f5598bd84f8a34e14d466b2298d5c14b0ecf5061
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-25 22:41:15 +1000

    home page

--- client/src/routes/index.lazy.tsx
+++ client/src/routes/index.lazy.tsx
@@ -1,13 +1,6 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import Hero from "~/features/hero/Hero";
 
 export const Route = createLazyFileRoute("/")({
-  component: Index,
+  component: Hero,
 });
-
-function Index() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  );
-}
