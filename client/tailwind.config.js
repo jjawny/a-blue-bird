2a422f7048b4a2dd56822b75aee84c8a6ac6a79c
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-27 10:54:28 +1000

    spin animation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,15 @@ export default {
         syne: ["Syne", "sans-serif"],
         whisper: ["Whisper", "sans-serif"],
       },
+      animation: {
+        spin: "spin 20s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
     },
   },
   plugins: [],
