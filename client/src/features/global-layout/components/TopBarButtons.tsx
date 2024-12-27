3494559de5f4fc963f713724052a87c95b11409d
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-27 22:36:11 +1000

    tooltip for tech box

--- client/src/features/global-layout/components/TopBarButtons.tsx
+++ client/src/features/global-layout/components/TopBarButtons.tsx
@@ -1,4 +1,4 @@
-import { IconButton, Link, Typography } from "@mui/material";
+import { IconButton, Link, Tooltip, Typography } from "@mui/material";
 import { useNavigate } from "@tanstack/react-router";
 import { memo } from "react";
 import { GoHome as HomeIcon } from "react-icons/go";
@@ -8,15 +8,17 @@ export default function TopBarButtons() {
 
   const TechnologyLinks = () => {
     return (
-      <div className="flex items-center gap-2 rounded-md border border-stone-200 px-2 py-1">
-        <Link href={"https://react.dev/"} target="_blank" rel="noopener noreferrer">
-          <img src="./images/react-logo.png" className="h-5 w-5 animate-spin rounded-full" />
-        </Link>
-        <Typography className="!font-whisper text-stone-600">x</Typography>
-        <Link href={"https://dotnet.microsoft.com/"} target="_blank" rel="noopener noreferrer">
-          <img src="./images/dotnet-mascot.png" className="h-5 w-5 rounded-full" />
-        </Link>
-      </div>
+      <Tooltip title="Made with React & .NET">
+        <div className="flex items-center gap-2 rounded-md border border-stone-200 px-2 py-1">
+          <Link href={"https://react.dev/"} target="_blank" rel="noopener noreferrer">
+            <img src="/images/react-logo.png" className="h-5 w-5 animate-spin" />
+          </Link>
+          <Typography className="!font-whisper text-stone-600">x</Typography>
+          <Link href={"https://dotnet.microsoft.com/"} target="_blank" rel="noopener noreferrer">
+            <img src="/images/dotnet-mascot.png" className="h-5 w-5 duration-150 hover:scale-105" />
+          </Link>
+        </div>
+      </Tooltip>
     );
   };
 
